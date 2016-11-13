003030a7896ec71a06254ed5a810ed212259d998
Devin Fallak <dmfallak@gmail.com>
2016-11-13 01:31:11 -0500

    Use ssl and change port number

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
 from simplerouter import Router
+import os
+import ssl
+
+PEM_FILE = os.environ['PEM_FILE']
 
 router = Router()
 
@@ -10,6 +14,9 @@ application = router.as_wsgi
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    print "Starting wedding server on port 8080"
-    make_server('', 8080, application).serve_forever()
+    print "Starting wedding server on port 4443"
+    https_server = make_server('', 4443, application)
+    conn = ssl.wrap_socket(https_server.socket, certfile=PEM_FILE, server_side=True)
+    https_server.socket = conn
+    https_server.serve_forever()
  
